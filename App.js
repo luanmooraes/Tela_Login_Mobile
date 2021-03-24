3625f930563c029a4d186b5cb2008e9883d2c78b
Luan Moraes <luansilvamoraess@gmail.com>
2021-03-24 12:40:57 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, TextInput, TouchableOpacity, Text, Alert } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.logo}
+        source={require('./src/assets/logo.png')}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu email"
+      />
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Digite sua senha"
+      />
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={()=>{Alert.alert("Cadastro","Você esta cadastrado!")}}
+        >
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor:'#3C096C',
+  },
+  
+  logo: {
+    
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+  },
+
+  input:{
+    backgroundColor: '#FFF',
+    marginTop: 10,
+    padding: 10,
+    width: 300,
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderRadius: 5,
+  },
+
+  button : {
+    width: 300,
+    height: 42,
+    backgroundColor: '#3498DB',
+    marginTop:10,
+    borderRadius: 4,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  buttonText : {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFF',
+  }
+
+
+
 });
+
+      
+
+
